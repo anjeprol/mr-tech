d222abb5834e67bdf526028b1bb5dca5f681048e
antonio prado <antonio.prado@amk-technologies.com>
2018-09-03 17:18:10 -0500

    add hide and show menu on tool bar

--- app/src/main/java/com/pramont/mrtecc/mrtech/ScrollingActivity.java
+++ app/src/main/java/com/pramont/mrtecc/mrtech/ScrollingActivity.java
@@ -10,7 +10,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +31,66 @@ public class ScrollingActivity extends AppCompatActivity {
         });
 
         AppBarLayout mAppBarLayout = findViewById(R.id.app_bar);
+        mAppBarLayout.addOnOffsetChangedListener(this);
     }
+
+    /**
+     * Called when the {@link AppBarLayout}'s layout offset has been changed. This allows
+     * child views to implement custom behavior based on the offset (for instance pinning a
+     * view at a certain y value).
+     *
+     * @param appBarLayout   the {@link AppBarLayout} which offset has changed
+     * @param verticalOffset the vertical offset for the parent {@link AppBarLayout}, in px
+     */
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        invalidateOptionsMenu();
+
+        //  Vertical offset == 0 indicates appBar is fully  expanded.
+        if (Math.abs(verticalOffset) > 200)
+        {
+            showOption(R.id.action_info);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        hideOption(R.id.action_info);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+        else if (id == R.id.action_info)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void hideOption(int id) {
+        MenuItem item = menu.findItem(id);
+        item.setVisible(false);
+    }
+
+    private void showOption(int id) {
+        MenuItem item = menu.findItem(id);
+        item.setVisible(true);
+    }
+
 }
