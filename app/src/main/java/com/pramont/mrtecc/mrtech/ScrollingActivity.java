71927f88d9476ae03842b79626fda2e1d89ef1d7
antonio prado <antonio.prado@amk-technologies.com>
2018-09-03 16:33:02 -0500

    update configurations

--- app/src/main/java/com/pramont/mrtecc/mrtech/ScrollingActivity.java
+++ app/src/main/java/com/pramont/mrtecc/mrtech/ScrollingActivity.java
@@ -1,11 +1,14 @@
 package com.pramont.mrtecc.mrtech;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -24,5 +27,7 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        AppBarLayout mAppBarLayout = findViewById(R.id.app_bar);
     }
 }
